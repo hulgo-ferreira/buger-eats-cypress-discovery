686e5944517bb1f1e9b6af2d3deda1e918bffe5a
Hulgo Rafael <hulgo.ferreira@fatec.sp.gov.br>
2022-02-04 20:48:25 -0300

    ajustando a suite home para os padrões do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,8 +2,7 @@
 
 describe('home page', () => {  //função javascript
     it('app deve estar online', () => {
-        cy.viewport(1440,900) //defindo o tamanho da resolução
-        cy.visit('https://buger-eats.vercel.app');
+        cy.visit('/');
         //validando se existe um texto na tag h1
     //esquema de busca: bisavô, avô, pai e filho #page-home main h1
         cy.get('#page-home main h1').should('have.text','Seja um parceiro entregador pela Buger Eats') 
